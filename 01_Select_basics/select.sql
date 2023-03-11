b4e2e9850c86c42ba1bdb37dbb4f1e316341ffe3
SURESH VJ <vijjanaramasuresh@gmail.com>
2023-03-11 22:20:17 +0530

    SQL BASICS

--- 01_Select_basics/select.sql
+++ 01_Select_basics/select.sql
@@ -4,5 +4,5 @@ SELECT
 FROM 
     titanic;
 
---- Output : [op](https://github.com/ramasureshvijjana/SQL_Basics/blob/master/01_Select_basics/01_Output_data_files/data-1678552337990.csv)
+ Output : https://github.com/ramasureshvijjana/SQL_Basics/blob/master/01_Select_basics/01_Output_data_files/data-1678552337990.csv
 
