2956eaf7aac4ea8c73ed2f4d8b2068e8bdfc3fef
SURESH VJ <vijjanaramasuresh@gmail.com>
2023-03-11 22:18:42 +0530

    SQL BASICS

--- 01_Select_basics/select.sql
+++ 01_Select_basics/select.sql
@@ -1,5 +1,8 @@
+-- Select two columns data
 SELECT 
     passengerid, pclass 
 FROM 
     titanic;
+
+--- Output : [op](https://github.com/ramasureshvijjana/SQL_Basics/blob/master/01_Select_basics/01_Output_data_files/data-1678552337990.csv)
 
