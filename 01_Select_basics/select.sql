a20b7d8d22189bffc25f733936863948bc2b3909
SURESH VJ <vijjanaramasuresh@gmail.com>
2024-02-06 21:54:52 +0530

    Create table

--- 01_Select_basics/select.sql
+++
@@ -1,8 +0,0 @@
--- Select two columns data
-SELECT 
-    passengerid, pclass 
-FROM 
-    titanic;
-
- Output : https://github.com/ramasureshvijjana/SQL_Basics/blob/master/01_Select_basics/01_Output_data_files/data-1678552337990.csv
-
---
+++ Creating_table_inserting_data/Create_a_empty_table.sql
@@ -0,0 +1,26 @@
+---------------------------------------------
+/*Creating a table*/
+---------------------------------------------
+CREATE TABLE Employee (
+    EmpID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255),
+	Sal float,
+	Experiance float
+);
+
+---------------------------------------------
+/*Insert data into table*/
+---------------------------------------------
+
+INSERT INTO employee
+VALUES (24001, 'Vijjana', 'Rama Suresh', 
+		'Gudapalli', 'Malikipuram',
+	   70000, 4.5);
+
+---------------------------------------------
+/*Creating a table*/
+---------------------------------------------
+
